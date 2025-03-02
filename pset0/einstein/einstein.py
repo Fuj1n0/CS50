83e651e4a986839cb9e08ac0a84d4ff2238b9233
Fuj1n0 <197345221+Fuj1n0@users.noreply.github.com>
2025-03-02 04:30:11 +0000

    week 0

---
+++ pset0/einstein/einstein.py
@@ -0,0 +1,4 @@
+c_square = 90000000000000000
+m = int(input("m: "))
+e = m * c_square
+print("E:",e)
---
+++ pset0/faces/faces.py
@@ -0,0 +1,3 @@
+user_input  = input()
+output = user_input.replace(":)", "🙂").replace(":(", "🙁")
+print(output)
